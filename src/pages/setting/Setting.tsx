752050fdd1cbefdd744f5cf3d70e6d2a8b7b4f5a
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2024-11-13 16:37:45 +0330

    checklogout

--- src/pages/setting/Setting.tsx
+++ src/pages/setting/Setting.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Setting = () => {
-  return <div>Setting</div>;
+  return (
+    <div>
+      <Link to="/login">logout</Link>
+    </div>
+  );
 };
 
 export default Setting;
